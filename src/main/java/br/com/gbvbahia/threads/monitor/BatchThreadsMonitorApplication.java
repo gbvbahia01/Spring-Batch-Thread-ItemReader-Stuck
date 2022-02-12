902b21e8d3b6f495bb6d88bf517a921d78b7529e
Guilherme Braga <gbvbahia01@gmail.com>
2022-02-12 08:55:54 -0300

    Initial Application Created
    A Spring batch application created

---
+++ src/main/java/br/com/gbvbahia/threads/monitor/BatchThreadsMonitorApplication.java
@@ -0,0 +1,13 @@
+package br.com.gbvbahia.threads.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchThreadsMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchThreadsMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gbvbahia/threads/monitor/BatchThreadsMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gbvbahia.threads.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchThreadsMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
